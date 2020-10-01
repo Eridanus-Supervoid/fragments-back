4d375258dbe12a963f61e661ab1fc99fd7b59fd6
eliot <eliot@pop-os.localdomain>
2020-10-01 16:17:59 -0500

    deploy

---
+++ public/build/precache-manifest.7f20fd3d8bafcfe66523b42e40f6d989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cb072290c0089813cc20594eed633b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be0eae34b0ad50bf9323",
+    "url": "/static/css/2.13b9b12e.chunk.css"
+  },
+  {
+    "revision": "0aa222f751dc6d30bd46",
+    "url": "/static/css/main.24b85401.chunk.css"
+  },
+  {
+    "revision": "be0eae34b0ad50bf9323",
+    "url": "/static/js/2.bed916e5.chunk.js"
+  },
+  {
+    "revision": "0aa222f751dc6d30bd46",
+    "url": "/static/js/main.9acaab8c.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
